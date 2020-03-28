341e84e0c6ad4e960aff82f15b52d50489a24717
Leaf26 <lxgoldsmith@gmail.com>
2020-03-28 16:21:18 -0400

    fix duplicate msg

--- src/main/java/bukkitTasks/OnSleepChecks.java
+++ src/main/java/bukkitTasks/OnSleepChecks.java
@@ -21,35 +21,40 @@ public class OnSleepChecks extends BukkitRunnable{
 	
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		//if player isn't sleeping anymore when the server gets here, pull out
 		if( !(this.player.isSleeping()) ) {
 			this.cancel();
 			return;
 		}
 		if(this.player.hasPermission("sleep.ignore")) return;
 		if(this.player.isSleepingIgnored()) return;
-		if(this.plugin.sleepingPlayers.get(this.player.getWorld()).size() > 0) return;
+		
+		//add player to list of sleeping players
 		this.plugin.sleepingPlayers.get(this.player.getWorld()).add(this.player);
+		
 		int numPlayers = 0;
 		int numSleepingPlayers = 0;
 		if(this.config.config.getBoolean("showMessageToOtherWorld")) {
+			//check all worlds for valid players
 			for (World w : (this.plugin.sleepingPlayers.keySet())){
-				for ( Player p : w.getPlayers()) {
-					if(!p.isSleepingIgnored() && !p.hasPermission("sleep.ignore"))
-						numPlayers = numPlayers + 1;
-				}
 				numSleepingPlayers = numSleepingPlayers	+ this.plugin.sleepingPlayers.get(w).size();
+				for ( Player p : w.getPlayers()) 
+					if(!p.isSleepingIgnored() && !p.hasPermission("sleep.ignore")) numPlayers = numPlayers + 1;
 			}
-			if(numSleepingPlayers < 2 && numPlayers > 1) new AnnounceSleep(this.plugin, this.config, this.player).runTaskAsynchronously(this.plugin);
 		}
 		else{
+			//check just this world for valid players
 			for ( Player p : this.player.getWorld().getPlayers()) {
 				if(!p.isSleepingIgnored() && !p.hasPermission("sleep.ignore"))
 					numPlayers = numPlayers + 1;
 			}
 			numSleepingPlayers = plugin.sleepingPlayers.get(this.player.getWorld()).size();
 		}
+		
+		//only announce the first bed entry, when there's more than one player to see it
 		if(numSleepingPlayers < 2 && numPlayers > 1) new AnnounceSleep(this.plugin, this.config, this.player).runTaskAsynchronously(this.plugin);
+		
+		//if not already doing sleep stuff in this world, start doing stuff
 		if(!plugin.doSleep.containsKey(this.player.getWorld())) {
 			plugin.doSleep.put(this.player.getWorld(), new PassTime(this.plugin, this.config, this.player.getWorld()).runTaskLater(this.plugin, config.config.getInt("sleepDelay")));
 		}
